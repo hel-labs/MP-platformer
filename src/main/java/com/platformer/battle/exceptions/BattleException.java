1b8bb711294c996d69af5dfee6813537d01d1a2a
not-hexil <155353385+not-hexil@users.noreply.github.com>
2026-04-17 11:28:20 +0600

    Scaffolding without code

--- src/main/java/com/platformer/battle/exceptions/BattleException.java
+++
@@ -1,7 +0,0 @@
-package com.echoes.exceptions;
-
-public class BattleException extends GameException {
-    public BattleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/platformer/battle/exceptions/EnemyAlreadyDeadException.java
+++
@@ -1,7 +0,0 @@
-package com.echoes.exceptions;
-
-public class EnemyAlreadyDeadException extends BattleException {
-    public EnemyAlreadyDeadException(String enemyName) {
-        super("Cannot act on defeated enemy: " + enemyName);
-    }
-}
